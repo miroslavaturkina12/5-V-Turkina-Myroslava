8c24f59204800776a1489d8ab230d9b21dc84eea
miroslavaturkina12 <miroslava.turkina12@gmail.com>
2026-05-18 08:05:33 +0000

    Зробила зірки

---
+++ exercise30.py
@@ -0,0 +1,14 @@
+from rurtle  import *
+from random  import *
+
+setup (400,400)
+bgcolor ('darkblue')
+colors  = ['white', 'yellow', 'orange' ]
+speed(0)
+
+for i in  range(1,100):
+  color (colors [i % 3])
+  up()
+  goto(randint(-200,200), randint(1,8))
+
+end_fill()
